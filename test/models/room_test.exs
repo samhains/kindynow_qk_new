cf78dbc4c061dca47cec14198410b71ebd2ffae3
sdlovecraft <sdh@eml.cc>
2016-06-07 09:47:05 +1000

    automatically generated model testS

---
+++ test/models/room_test.exs
@@ -0,0 +1,18 @@
+defmodule KindynowQkNew.RoomTest do
+  use KindynowQkNew.ModelCase
+
+  alias KindynowQkNew.Room
+
+  @valid_attrs %{capacity: 42, casual_booking_type: "some content", max_age: 42, min_age: 42, name: "some content"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = Room.changeset(%Room{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = Room.changeset(%Room{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
---
+++ test/models/service_test.exs
@@ -0,0 +1,18 @@
+defmodule KindynowQkNew.ServiceTest do
+  use KindynowQkNew.ModelCase
+
+  alias KindynowQkNew.Service
+
+  @valid_attrs %{address_post_code: "some content", address_state: "some content", address_street: "some content", address_suburb: "some content", email: "some content", licensed_capacity: "some content", name: "some content", phone_number: "some content", time_zone: "some content"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = Service.changeset(%Service{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = Service.changeset(%Service{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
