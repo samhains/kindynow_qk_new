2479349187d14adb535d0955794b5085877e38be
sdlovecraft <sdh@eml.cc>
2016-07-15 15:51:55 +1000

    add the basis for booking and attendance saving

--- web/jobs/update_bookings_and_openings.ex
+++ web/jobs/update_bookings_and_openings.ex
@@ -3,6 +3,10 @@ defmodule KindynowQkNew.UpdateBookingsAndOpenings do
   use Timex
   alias KindynowQkNew.Service
   alias KindynowQkNew.Room
+  alias KindynowQkNew.Child
+  alias KindynowQkNew.Availability
+  alias KindynowQkNew.Booking
+  alias KindynowQkNew.BookingType
   alias KindynowQkNew.Repo
   alias KindynowQkNew.QkApi
 
@@ -37,5 +41,50 @@ defmodule KindynowQkNew.UpdateBookingsAndOpenings do
       rooms
       |> Enum.reduce(%{}, fn(room, total) -> Map.put(total, room.id, bookings_data[room.sync_id]) end)
 
+    bookings_map
+      |> Enum.each(fn ({room_id, room_bookings}) ->
+        Enum.each(room_bookings, fn({date_string, room_hash})->
+          if date_string != "$id" do
+            date = Timex.parse!(date_string, "%FT%T", :strftime)
+
+            attendance = %Availability{
+              date: date,
+              room_id: room_id,
+              service_id: service_id,
+              used: room_hash["UtilisedPlaces"],
+              capacity: room_hash["PlaceLimit"],
+              open: room_hash["RollOpenStatus"] == 0,
+            }
+
+            # date = Ecto.DateTime.now
+            Enum.each(room_hash["ChildSyncIdChildDateValueMap"], fn({child_sync_id, booking_hash}) ->
+              if child_sync_id != "$id" do
+
+                sync_id = child_sync_id
+
+                status = booking_hash["DayStatus"];
+                # need to check if booking is already an opening
+
+                IEx.pry
+                child = Repo.one(from c in Child, where: c.sync_id == ^sync_id)
+                # qk_booking_id = Timex.format!(date, "%FT%T", :strftime) <> ":" <> room_id <> ":" <> child.id
+                # find booking first
+                booking  = %Booking{
+                  date: date,
+                  # qk_booking_id: qk_booking_id,
+                  room_id: room_id,
+                  # child_id: child.id,
+                  utilisation: booking_hash["Utilisation"],
+                  permanent_booking: booking_hash["PermanentBooking"],
+                  absent: status == 2,
+                  day_status: status,
+                  service_id: service_id
+                }
+              end
+
+            end)
+          end
+        end)
+      end)
   end
 end
